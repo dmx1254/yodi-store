f4e5ae127dbafc2a977ad214c5f20192749684ba
mamadou SY <tipox1254@gmail.com>
2025-12-17 12:07:57 +0000

    Changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://yodistore.vercel.app'
+  const baseUrl = 'https://yyodi-store.com'
   const currentDate = new Date().toISOString().split('T')[0]
 
   // Pages statiques principales
